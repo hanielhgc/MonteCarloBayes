22453938021e2375e850836b586370d30f37c11a
hanielhgc <haniel_sasuke@hotmail.com>
2020-05-11 23:26:29 -0300

    segundo commit

---
+++ EP13PPE/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EP13PPE
+{
+    public class Node
+    {
+
+        public string nome { get; set; }
+        public List<Node> pais { get; set; }
+        public bool? valor { get; set; }
+        public string[,] tabela { get; set; }
+
+    }
+}
--- EP13PPE/Program.cs
+++ EP13PPE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EP13PPE
 {
@@ -6,7 +7,53 @@ namespace EP13PPE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Criando e atribuindo valores aos nós da rede
+
+            List<Node> nos = new List<Node>();
+
+            Node no = new Node();
+            no.nome = "1-Burglary";
+            no.pais = new List<Node>();
+            no.tabela = new string[1, 1] { { "0,001" } };
+
+            nos.Add(no);
+
+            Node no2 = new Node();
+            no2.nome = "2-Earthquake";
+            no2.pais = new List<Node>();
+            no2.tabela = new string[1, 1] { { "0,002" } };
+
+            nos.Add(no2);
+
+
+            Node no3 = new Node();
+            no3.nome = "3-Alarm";
+            no3.pais = new List<Node>();
+            no.pais.Add(no);
+            no.pais.Add(no2);
+            no3.tabela = new string[4, 3] {{"true","true","0,95"},
+                                          {"true","false","0,94"},
+                                          {"false","true","0,29"},
+                                          {"false","false","0,001"} };
+
+
+            Node no4 = new Node();
+            no4.nome = "4-JohnCalls";
+            no4.pais = new List<Node>();
+            no4.pais.Add(no3);
+            no4.tabela = new string[2, 2] { { "true",  "0,90" },
+                                            { "false" ,"0,05"}};
+
+
+            Node no5 = new Node();
+            no5.nome = "5-MaryCalls";
+            no5.pais = new List<Node>();
+            no5.pais.Add(no3);
+            no5.tabela = new string[2, 2] { { "true",  "0,70" },
+                                            { "false" ,"0,01"}};
+
+
         }
     }
 }
